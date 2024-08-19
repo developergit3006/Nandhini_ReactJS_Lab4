7935a2ea7145bcf6bcb4524c286e67f011c32c17
developergit3006 <152990830+developergit3006@users.noreply.github.com>
2024-08-19 14:27:48 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { ExpenseSplitterApp } from "./components/ExpenseSplitterApp";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+      <ExpenseSplitterApp></ExpenseSplitterApp>
+    </div>
+  );
+}
+
+export default App;
